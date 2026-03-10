4b684cfab87cc41048d716a22850ab95135de36f
gamer-2000 <fotbol668@gmail.com>
2026-03-10 07:52:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,111 @@
 const mineflayer = require('mineflayer')
-const {pathfinder, Movements, goals} = require('mineflayer-pathfinder')
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
 const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req,res)=>res.send("bot running"))
-app.listen(PORT, ()=>console.log("Web server running"))
+app.get('/', (req, res) => res.send("AFK bot running"))
+app.listen(PORT, () => console.log("Web server running"))
 
-const names = ["ShadowAxe","NovaPvP","PixelRaider","VoidCrafter","IronSpecter"]
-const username = names[Math.floor(Math.random()*names.length)]
+const names = [
+  "ShadowAxe",
+  "NovaPvP",
+  "PixelRaider",
+  "VoidCrafter",
+  "IronSpecter",
+  "NightMiner"
+]
 
-function createBot(){
+function createBot() {
 
-const bot = mineflayer.createBot({
-  host: 'spydimc.falix.me',
-  username: username,
-  version: '1.21',
-  auth: 'offline'
-})
+  const username = names[Math.floor(Math.random() * names.length)]
 
-bot.loadPlugin(pathfinder)
+  const bot = mineflayer.createBot({
+    host: 'spydimc.falix.me',
+    username: username,
+    version: '1.21',
+    auth: 'offline'
+  })
 
-bot.once('spawn', () => {
+  bot.loadPlugin(pathfinder)
 
-  console.log("Bot joined as", username)
+  bot.once('spawn', () => {
 
-  const mcData = require('minecraft-data')(bot.version)
-  const defaultMove = new Movements(bot, mcData)
+    console.log("Bot joined as", username)
 
-  bot.pathfinder.setMovements(defaultMove)
+    const mcData = require('minecraft-data')(bot.version)
+    const defaultMove = new Movements(bot, mcData)
 
-  setInterval(() => {
+    bot.pathfinder.setMovements(defaultMove)
 
-    const x = bot.entity.position.x + (Math.random()*20 - 10)
-    const z = bot.entity.position.z + (Math.random()*20 - 10)
-    const y = bot.entity.position.y
+    // Walk randomly around
+    setInterval(() => {
 
-    const goal = new goals.GoalBlock(Math.floor(x), Math.floor(y), Math.floor(z))
+      const x = bot.entity.position.x + (Math.random() * 20 - 10)
+      const z = bot.entity.position.z + (Math.random() * 20 - 10)
+      const y = bot.entity.position.y
 
-    bot.pathfinder.setGoal(goal)
+      const goal = new goals.GoalBlock(
+        Math.floor(x),
+        Math.floor(y),
+        Math.floor(z)
+      )
 
-  }, 30000)
+      bot.pathfinder.setGoal(goal)
 
-  // look around sometimes
-  setInterval(()=>{
-    bot.look(Math.random()*Math.PI*2,(Math.random()-0.5)*Math.PI)
-  },20000)
+    }, 30000)
 
-  // occasional chat
-  setInterval(()=>{
-    const msgs = ["hi","anyone here","hello","lol","hmm"]
-    const msg = msgs[Math.floor(Math.random()*msgs.length)]
-    bot.chat(msg)
-  },300000)
+    // Look around
+    setInterval(() => {
+      const yaw = Math.random() * Math.PI * 2
+      const pitch = (Math.random() - 0.5) * Math.PI
+      bot.look(yaw, pitch)
+    }, 20000)
 
-})
+    // Random jump
+    setInterval(() => {
+      bot.setControlState('jump', true)
+      setTimeout(() => bot.setControlState('jump', false), 500)
+    }, 25000)
 
-bot.on('end',()=>{
-  console.log("Disconnected, reconnecting...")
-  setTimeout(createBot,15000)
-})
+    // Occasional chat
+    setInterval(() => {
+      const msgs = [
+        "hi",
+        "hello",
+        "anyone here?",
+        "lol",
+        "hmm"
+      ]
+      const msg = msgs[Math.floor(Math.random() * msgs.length)]
+      bot.chat(msg)
+    }, 300000)
 
-bot.on('kicked',reason=>console.log("Kicked:",reason))
-bot.on('error',err=>console.log("Error:",err))
+  })
+
+  // Death detection
+  bot.on('death', () => {
+    console.log("Bot died 💀 waiting for respawn")
+  })
+
+  bot.on('respawn', () => {
+    console.log("Bot respawned")
+  })
+
+  // Reconnect if disconnected
+  bot.on('end', () => {
+    console.log("Disconnected. Reconnecting in 15s...")
+    setTimeout(createBot, 15000)
+  })
+
+  bot.on('kicked', (reason) => {
+    console.log("Kicked:", reason)
+  })
+
+  bot.on('error', (err) => {
+    console.log("Error:", err)
+  })
 
 }
 
