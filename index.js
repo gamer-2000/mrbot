6d511bb70d8a52d95458efabe3d32d42f85625ce
gamer-2000 <fotbol668@gmail.com>
2026-03-10 07:44:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,69 @@
 const mineflayer = require('mineflayer')
+const {pathfinder, Movements, goals} = require('mineflayer-pathfinder')
 const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req,res)=>{
-  res.send("AFK bot running")
-})
+app.get('/', (req,res)=>res.send("bot running"))
+app.listen(PORT, ()=>console.log("Web server running"))
 
-app.listen(PORT,()=>{
-  console.log("Web server running")
-})
+const names = ["ShadowAxe","NovaPvP","PixelRaider","VoidCrafter","IronSpecter"]
+const username = names[Math.floor(Math.random()*names.length)]
 
 function createBot(){
 
-  const bot = mineflayer.createBot({
-    host: 'spydimc.falix.me',
-    username: 'AFK_Bot',
-    version: '1.21',
-    auth: 'offline'
-  })
+const bot = mineflayer.createBot({
+  host: 'spydimc.falix.me',
+  username: username,
+  version: '1.21',
+  auth: 'offline'
+})
 
-  bot.on('spawn',()=>{
-    console.log("Bot joined server")
+bot.loadPlugin(pathfinder)
 
-    setTimeout(()=>{
+bot.once('spawn', () => {
 
-      setInterval(()=>{
+  console.log("Bot joined as", username)
 
-        const actions = ['forward','back','left','right','jump']
-        const action = actions[Math.floor(Math.random()*actions.length)]
+  const mcData = require('minecraft-data')(bot.version)
+  const defaultMove = new Movements(bot, mcData)
 
-        bot.setControlState(action,true)
+  bot.pathfinder.setMovements(defaultMove)
 
-        setTimeout(()=>{
-          bot.setControlState(action,false)
-        },2000)
+  setInterval(() => {
 
-      },15000)
+    const x = bot.entity.position.x + (Math.random()*20 - 10)
+    const z = bot.entity.position.z + (Math.random()*20 - 10)
+    const y = bot.entity.position.y
 
-      setInterval(()=>{
-        bot.look(Math.random()*Math.PI*2,(Math.random()-0.5)*Math.PI)
-      },20000)
+    const goal = new goals.GoalBlock(Math.floor(x), Math.floor(y), Math.floor(z))
 
-    },10000)
+    bot.pathfinder.setGoal(goal)
 
-  })
+  }, 30000)
 
-  bot.on('kicked',(reason)=>{
-    console.log("Kicked:",reason)
-  })
+  // look around sometimes
+  setInterval(()=>{
+    bot.look(Math.random()*Math.PI*2,(Math.random()-0.5)*Math.PI)
+  },20000)
 
-  bot.on('end',()=>{
-    console.log("Disconnected. Reconnecting in 15s...")
-    setTimeout(createBot,15000)
-  })
+  // occasional chat
+  setInterval(()=>{
+    const msgs = ["hi","anyone here","hello","lol","hmm"]
+    const msg = msgs[Math.floor(Math.random()*msgs.length)]
+    bot.chat(msg)
+  },300000)
 
-  bot.on('error',(err)=>{
-    console.log("Error:",err)
-  })
+})
+
+bot.on('end',()=>{
+  console.log("Disconnected, reconnecting...")
+  setTimeout(createBot,15000)
+})
+
+bot.on('kicked',reason=>console.log("Kicked:",reason))
+bot.on('error',err=>console.log("Error:",err))
 
 }
 
